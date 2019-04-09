adf7ad76f3164551ca284f98b965a97c8628920a
cohadar <cohadar@gmail.com>
2019-04-09 17:13:51 +0200

    ease

--- grimms_märchen/04-words.sh
+++ grimms_märchen/04-words.sh
@@ -19,3 +19,12 @@ cat "$tempfile" |
 
 # do a pareto here
 head -$(cut -f1 04-words.tsv | python 04-pareto-index.py) 04-words.tsv > 04-words.pareto.tsv
+
+# calculate line ease
+ls tmp/03 |
+while read -r PAGE; do
+	echo "ease $PAGE"
+	cat tmp/03/"$PAGE" |
+		python 04-line-ease.py \
+		> tmp/04/"$PAGE"
+done
