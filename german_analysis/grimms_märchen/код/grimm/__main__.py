57e0a2748c8bc5baab1662ab46438ebb3cd9313a
Damir Čohadarević <cohadar@gmail.com>
2020-04-28 08:31:48 +0200

    likz

--- german_analysis/grimms_märchen/код/grimm/__main__.py
+++ german_analysis/grimms_märchen/код/grimm/__main__.py
@@ -3,14 +3,20 @@ from bs4 import BeautifulSoup
 СПИСАК = "https://www.grimmstories.com/de/grimm_maerchen/list"
 
 
+def скини_списак(сесија, урл):
+    одг = сесија.дај(урл)
+    одг.добар()
+    супа = BeautifulSoup(одг.текст, 'html.parser')
+    return [линк.get('href') for линк in супа.body.find_all('ul')[1].find_all('a')]
+
+
 def главна():
     к = Контејнер()
     сесија = к.сесија()
-    одг = сесија.дај(СПИСАК)
-    одг.добар()
-    супа = BeautifulSoup(одг.текст, 'html.parser')
-    for линк in супа.body.find_all('ul')[1].find_all('a'):
-        print(линк.get('href'))
+    списак = скини_списак(сесија, СПИСАК)
+    for линк in списак:
+        print(линк)
+        сесија.дај(линк)
 
 
 if __name__ == '__main__':
