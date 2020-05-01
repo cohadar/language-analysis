cfa08b6dd161b290b36d996d36c7049c3418fcaa
Damir Čohadarević <cohadar@gmail.com>
2020-05-01 18:45:30 +0200

    meh

--- german_analysis/grimms_märchen/тестови/test_линајзер.py
+++ german_analysis/grimms_märchen/тестови/test_линајзер.py
@@ -53,67 +53,37 @@ def test_почетни_цитат_не_ломи():
     assert л(текст) == '„Dobar dan!“'
 
 
-# def test_директан_говор_ломљење_двотачке():
-#     текст = '"Dobar dan: Kako ste?"'
-#     assert л(текст) == '„Dobar dan:“\n„Kako ste?“'
+def test_директан_говор_ломљење_двотачке():
+    текст = '"Dobar dan: Kako ste?"'
+    assert л(текст) == '„Dobar dan:“\n„Kako ste?“'
 
 
-# def test_директан_говор():
-#     текст = 'der sprach: "Bruder Hund. warum bist du! so traurig?"'
-#     линије = тл(текст)
-#     assert len(линије) == 4
-#     assert линије[0] == 'der sprach:'
-#     assert линије[1] == '"Bruder Hund."'
-#     assert линије[2] == '"warum bist du!"'
-#     assert линије[3] == '"so traurig?"'
+def test_директан_говор():
+    текст = 'der sprach: "Bruder Hund. warum bist du! so traurig?"'
+    assert л(текст) == 'der sprach:\n„Bruder Hund.“\n„warum bist du!“\n„so traurig?“'
 
 
-# def test_директан_говор_ломи_линију_након():
-#     текст = 'zum Hunde: "Da bleib stehen, ich will dir unterpicken," setzte sich auf den Laden'
-#     линије = тл(текст)
-#     assert len(линије) == 4
-#     assert линије[0] == 'zum Hunde:'
-#     assert линије[1] == '"Da bleib stehen,"'
-#     assert линије[2] == '"ich will dir unterpicken,"'
-#     assert линије[3] == 'setzte sich auf den Laden'
+def test_директан_говор_ломи_линију_након():
+    текст = 'zum Hunde: "Da bleib stehen, ich will dir unterpicken," setzte sich auf den Laden'
+    assert л(текст) == 'zum Hunde:\n„Da bleib stehen,“\n„ich will dir unterpicken,“\nsetzte sich auf den Laden'
 
 
-# def test_директан_говор_ломи_линију_са_узвиком():
-#     текст = '"Ach, ich armer Mann!" rief er.'
-#     линије = тл(текст)
-#     assert len(линије) == 4
-#     assert линије[0] == '"Ach,"'
-#     assert линије[1] == '"ich armer Mann!"'
-#     assert линије[2] == 'rief er.'
-#     assert линије[3] == ''
+def test_директан_говор_ломи_линију_са_узвиком():
+    текст = '"Ach, ich armer Mann!" rief er.'
+    assert л(текст) == '„Ach,“\n„ich armer Mann!“\nrief er.'
 
 
-# def test_директан_говор_ломи_линију_са_питањем():
-#     текст = '"Bruder Hund, warum bist du so traurig?" Antwortete der Hund: "Ich bin hungrig."'
-#     линије = тл(текст)
-#     assert len(линије) == 5
-#     assert линије[0] == '"Bruder Hund,"'
-#     assert линије[1] == '"warum bist du so traurig?"'
-#     assert линије[2] == 'Antwortete der Hund:'
-#     assert линије[3] == '"Ich bin hungrig."'
-#     assert линије[4] == ''
+def test_директан_говор_ломи_линију_са_питањем():
+    текст = '"Bruder Hund, warum bist du so traurig?" Antwortete der Hund: "Ich bin hungrig."'
+    assert л(текст) == '„Bruder Hund,“\n„warum bist du so traurig?“\nAntwortete der Hund:\n„Ich bin hungrig.“'
 
 
-# def test_директан_говор_ломи_линију_са_тачком():
-#     текст = '"Bruder Hund, warum bist du so traurig." Antwortete der Hund: "Ich bin hungrig."'
-#     линије = тл(текст)
-#     assert len(линије) == 6
-#     assert линије[0] == '"Bruder Hund,"'
-#     assert линије[1] == '"warum bist du so traurig."'
-#     assert линије[2] == ''
-#     assert линије[3] == 'Antwortete der Hund:'
-#     assert линије[4] == '"Ich bin hungrig."'
-#     assert линије[5] == ''
+def test_директан_говор_ломи_линију_са_тачком():
+    текст = '"Bruder Hund, warum bist du so traurig." Antwortete der Hund: "Ich bin hungrig."'
+    assert л(текст) == '„Bruder Hund,“\n„warum bist du so traurig.“\nAntwortete der Hund:\n„Ich bin hungrig.“'
 
 
-# def test_директан_говор_тачка_ломи_јаче():
-#     текст = '"trla baba lan." da joj prodje dan'
-#     линије = тл(текст)
-#     assert линије == ['"trla baba lan."', '', 'da joj prodje dan']
-
+def test_директан_говор_тачка_ломи_јаче():
+    текст = '"trla baba lan." da joj prodje dan'
+    assert л(текст) == '„trla baba lan.“\nda joj prodje dan'
 
