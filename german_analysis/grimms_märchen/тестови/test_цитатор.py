3cfabe40be6f84b8a6cf83a44adb1605023696bc
Damir Čohadarević <cohadar@gmail.com>
2020-05-03 13:41:50 +0200

    ajmo

--- german_analysis/grimms_märchen/тестови/test_цитатор.py
+++ german_analysis/grimms_märchen/тестови/test_цитатор.py
@@ -18,11 +18,6 @@ def test_даш_трансформација():
     assert ц(текст) == ' – '
 
 
-def test_даш_трансформација2():
-    текст = '"dir geben?" -"Sie braucht"'
-    assert ц(текст) == '„dir geben?“ –„Sie braucht“'
-
-
 def test_црта_на_крају_речи1():
     текст = 'vier- bis fünfhundertfältig'
     assert ц(текст) == 'vier- bis fünfhundertfältig'
@@ -43,153 +38,164 @@ def test_већ_цитирано():
     assert ц(текст) == текст
 
 
-def test_на_крају_текста1():
-    текст = '"Deine Mutter"'
-    assert ц(текст) == '„Deine Mutter“'
+def test_најчешћи_наводници():
+    текст = 'sagte: "Hallo Mann." Er'
+    assert ц(текст) == 'sagte: „Hallo Mann.“ Er'
 
 
-def test_на_крају_текста2():
-    текст = '"der Hund"'
-    assert ц(текст) == '„der Hund“'
+
+# def test_даш_трансформација2():
+#     текст = '"dir geben?" -"Sie braucht"'
+#     assert ц(текст) == '„dir geben?“ –„Sie braucht“'
 
 
-def test_на_крају_текста3():
-    текст = '"zwiter lied."'
-    assert ц(текст) == '„zwiter lied.“'
+# def test_на_крају_текста1():
+#     текст = '"Deine Mutter"'
+#     assert ц(текст) == '„Deine Mutter“'
 
 
-def test_затворен_цитат1():
-    текст = '"spazieren gehen." Da'
-    assert ц(текст) == '„spazieren gehen.“ Da'
+# def test_на_крају_текста2():
+#     текст = '"der Hund"'
+#     assert ц(текст) == '„der Hund“'
 
 
-def test_затворен_цитат2():
-    текст = '"spazieren gehen," Da'
-    assert ц(текст) == '„spazieren gehen,“ Da'
+# def test_на_крају_текста3():
+#     текст = '"zwiter lied."'
+#     assert ц(текст) == '„zwiter lied.“'
 
 
-def test_затворен_цитат3():
-    текст = '"erste lied."\n"zwiter lied."'
-    assert ц(текст) == '„erste lied.“\n„zwiter lied.“'
+# def test_затворен_цитат1():
+#     текст = '"spazieren gehen." Da'
+#     assert ц(текст) == '„spazieren gehen.“ Da'
 
 
-def test_затворен_цитат4():
-    текст = '"Wer war das?" - ich.'
-    assert ц(текст) == '„Wer war das?“ – ich.'
+# def test_затворен_цитат2():
+#     текст = '"spazieren gehen," Da'
+#     assert ц(текст) == '„spazieren gehen,“ Da'
 
 
-def test_затворен_цитат5():
-    текст = '"Das soll dir schlecht bekommen." - Meh'
-    assert ц(текст) == '„Das soll dir schlecht bekommen.“ – Meh'
+# def test_затворен_цитат3():
+#     текст = '"erste lied."\n"zwiter lied."'
+#     assert ц(текст) == '„erste lied.“\n„zwiter lied.“'
 
 
-def test_затворен_цитат6():
-    текст = '"Laß dein Haar herunter."\nso ließ sie die Haare hinab.'
-    assert ц(текст) == '„Laß dein Haar herunter.“\nso ließ sie die Haare hinab.'
+# def test_затворен_цитат4():
+#     текст = '"Wer war das?" - ich.'
+#     assert ц(текст) == '„Wer war das?“ – ich.'
 
 
-def test_затворен_цитат7():
-    текст = '"doch betrogen!" In ihrem'
-    assert ц(текст) == '„doch betrogen!“ In ihrem'
+# def test_затворен_цитат5():
+#     текст = '"Das soll dir schlecht bekommen." - Meh'
+#     assert ц(текст) == '„Das soll dir schlecht bekommen.“ – Meh'
 
 
-def test_затворен_цитат8():
-    текст = '"Wie lauten die drei Fragen?" Der König sagte'
-    assert ц(текст) == '„Wie lauten die drei Fragen?“ Der König sagte'
+# def test_затворен_цитат6():
+#     текст = '"Laß dein Haar herunter."\nso ließ sie die Haare hinab.'
+#     assert ц(текст) == '„Laß dein Haar herunter.“\nso ließ sie die Haare hinab.'
 
 
-def test_затворен_цитат9():
-    текст = '"Ewigkeit vorbei."\n\n'
-    assert ц(текст) == '„Ewigkeit vorbei.“\n\n'
+# def test_затворен_цитат7():
+#     текст = '"doch betrogen!" In ihrem'
+#     assert ц(текст) == '„doch betrogen!“ In ihrem'
 
 
-def test_затворен_сумњив1():
-    текст = '"Das sollst du alles haben" sprach das Männchen'
-    assert ц(текст) == '„Das sollst du alles haben“ sprach das Männchen'
+# def test_затворен_цитат8():
+#     текст = '"Wie lauten die drei Fragen?" Der König sagte'
+#     assert ц(текст) == '„Wie lauten die drei Fragen?“ Der König sagte'
 
 
-def test_на_почетку_текста():
-    текст = '"Hallo Leute"'
-    assert ц(текст) == '„Hallo Leute“'
+# def test_затворен_цитат9():
+#     текст = '"Ewigkeit vorbei."\n\n'
+#     assert ц(текст) == '„Ewigkeit vorbei.“\n\n'
 
 
-def test_након_двотачке1():
-    текст = 'sagte: "Damit"'
-    assert ц(текст) == 'sagte: „Damit“'
+# def test_затворен_сумњив1():
+#     текст = '"Das sollst du alles haben" sprach das Männchen'
+#     assert ц(текст) == '„Das sollst du alles haben“ sprach das Männchen'
 
 
-def test_након_двотачке2():
-    текст = 'sagte: "Damit ich"'
-    assert ц(текст) == 'sagte: „Damit ich“'
+# def test_на_почетку_текста():
+#     текст = '"Hallo Leute"'
+#     assert ц(текст) == '„Hallo Leute“'
 
 
-def test_након_двотачке3():
-    текст = 'sprach zum Schneiderlein: "Mein, beiß"'
-    assert ц(текст) == 'sprach zum Schneiderlein: „Mein, beiß“'
+# def test_након_двотачке1():
+#     текст = 'sagte: "Damit"'
+#     assert ц(текст) == 'sagte: „Damit“'
 
 
-def test_након_двотачке4():
-    текст = 'haben und sprach: " Nun, weißt du"'
-    assert ц(текст) == 'haben und sprach: „ Nun, weißt du“'
+# def test_након_двотачке2():
+#     текст = 'sagte: "Damit ich"'
+#     assert ц(текст) == 'sagte: „Damit ich“'
 
 
-def test_спојена_двотачка1():
-    текст = 'nochmals und sagte:"Wenn ich"'
-    assert ц(текст) == 'nochmals und sagte:„Wenn ich“'
+# def test_након_двотачке3():
+#     текст = 'sprach zum Schneiderlein: "Mein, beiß"'
+#     assert ц(текст) == 'sprach zum Schneiderlein: „Mein, beiß“'
 
 
-def test_након_зареза1():
-    текст = 'sagte der Hirt, "Hello Mark"'
-    assert ц(текст) == 'sagte der Hirt, „Hello Mark“'
+# def test_након_двотачке4():
+#     текст = 'haben und sprach: " Nun, weißt du"'
+#     assert ц(текст) == 'haben und sprach: „ Nun, weißt du“'
 
 
-def test_након_зареза2():
-    текст = 'der Junge, " die Bank ist mein."'
-    assert ц(текст) == 'der Junge, „ die Bank ist mein.“'
+# def test_спојена_двотачка1():
+#     текст = 'nochmals und sagte:"Wenn ich"'
+#     assert ц(текст) == 'nochmals und sagte:„Wenn ich“'
 
 
-def test_након_црте1():
-    текст = 'Mann - "Ach," antwortete sie'
-    assert ц(текст) == 'Mann – „Ach,“ antwortete sie'
+# def test_након_зареза1():
+#     текст = 'sagte der Hirt, "Hello Mark"'
+#     assert ц(текст) == 'sagte der Hirt, „Hello Mark“'
 
 
-def test_након_црте2():
-    текст = '"Augenblick bei mir." - "Ach du"'
-    assert ц(текст) == '„Augenblick bei mir.“ – „Ach du“'
+# def test_након_зареза2():
+#     текст = 'der Junge, " die Bank ist mein."'
+#     assert ц(текст) == 'der Junge, „ die Bank ist mein.“'
 
 
-def test_након_тачке1():
-    текст = 'sich stehen. "Wie kannst"'
-    assert ц(текст) == 'sich stehen. „Wie kannst“'
+# def test_након_црте1():
+#     текст = 'Mann - "Ach," antwortete sie'
+#     assert ц(текст) == 'Mann – „Ach,“ antwortete sie'
 
 
-def test_након_тачке2():
-    текст = 'Blicken ansah. "Aha," rief sie'
-    assert ц(текст) == 'Blicken ansah. „Aha,“ rief sie'
+# def test_након_црте2():
+#     текст = '"Augenblick bei mir." - "Ach du"'
+#     assert ц(текст) == '„Augenblick bei mir.“ – „Ach du“'
 
 
-def test_након_тачке3():
-    текст = 'und fiel vor ihn hin. "Heda!"'
-    assert ц(текст) == 'und fiel vor ihn hin. „Heda!“'
+# def test_након_тачке1():
+#     текст = 'sich stehen. "Wie kannst"'
+#     assert ц(текст) == 'sich stehen. „Wie kannst“'
 
 
-def test_након_речи():
-    текст = 'und sprach "meine Stiefmutter"'
-    assert ц(текст) == 'und sprach „meine Stiefmutter“'
+# def test_након_тачке2():
+#     текст = 'Blicken ansah. "Aha," rief sie'
+#     assert ц(текст) == 'Blicken ansah. „Aha,“ rief sie'
 
 
-def test_нова_линија1():
-    текст = 'sagte:\n"Hallo, Mark"'
-    assert ц(текст) == 'sagte:\n„Hallo, Mark“'
+# def test_након_тачке3():
+#     текст = 'und fiel vor ihn hin. "Heda!"'
+#     assert ц(текст) == 'und fiel vor ihn hin. „Heda!“'
 
 
-def test_нова_линија2():
-    текст = '"erste lied."\n"zwiter lied."'
-    assert ц(текст) == '„erste lied.“\n„zwiter lied.“'
+# def test_након_речи():
+#     текст = 'und sprach "meine Stiefmutter"'
+#     assert ц(текст) == 'und sprach „meine Stiefmutter“'
 
 
-def test_нова_линија3():
-    текст = 'und sprach:\n"Mut heruet na myne Maegt"'
-    assert ц(текст) == 'und sprach:\n„Mut heruet na myne Maegt“'
+# def test_нова_линија1():
+#     текст = 'sagte:\n"Hallo, Mark"'
+#     assert ц(текст) == 'sagte:\n„Hallo, Mark“'
+
+
+# def test_нова_линија2():
+#     текст = '"erste lied."\n"zwiter lied."'
+#     assert ц(текст) == '„erste lied.“\n„zwiter lied.“'
+
+
+# def test_нова_линија3():
+#     текст = 'und sprach:\n"Mut heruet na myne Maegt"'
+#     assert ц(текст) == 'und sprach:\n„Mut heruet na myne Maegt“'
 
 
