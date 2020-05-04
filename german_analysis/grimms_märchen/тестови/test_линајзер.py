2638429c814831bdef1c78e537572ee05f91d9e5
Damir Čohadarević <cohadar@gmail.com>
2020-05-04 23:09:21 +0200

    meh

--- german_analysis/grimms_märchen/тестови/test_линајзер.py
+++ german_analysis/grimms_märchen/тестови/test_линајзер.py
@@ -13,69 +13,69 @@ def ли(текст):
     return лин.линије
 
 
-# def test_реч():
-#     текст = 'trläÄ'
-#     assert ли(текст) == [текст]
+def test_реч():
+    текст = 'trläÄ'
+    assert ли(текст) == [текст]
 
 
-# def test_једна_линија():
-#     текст = 'trläÄ böÖba lanßüÜ meh'
-#     assert ли(текст) == [текст]
+def test_једна_линија():
+    текст = 'trläÄ böÖba lanßüÜ meh'
+    assert ли(текст) == [текст]
 
 
-# def test_исте_линије():
-#     текст = 'trläÄ\nböÖba lanßüÜ\nmeh'
-#     assert ли(текст) == ['trläÄ\n', 'böÖba lanßüÜ\n', 'meh']
+def test_исте_линије():
+    текст = 'trläÄ\nböÖba lanßüÜ\nmeh'
+    assert ли(текст) == ['trläÄ\n', 'böÖba lanßüÜ\n', 'meh']
 
 
-# def test_линија_крај():
-#     текст = 'trläÄ\n'
-#     assert ли(текст) == [текст]
+def test_линија_крај():
+    текст = 'trläÄ\n'
+    assert ли(текст) == [текст]
 
 
-# def test_линија_почетак():
-#     текст = '\ntrläÄ'
-#     assert ли(текст) == ['\n', 'trläÄ']
+def test_линија_почетак():
+    текст = '\ntrläÄ'
+    assert ли(текст) == ['\n', 'trläÄ']
 
 
-# def test_крајеви_реченица():
-#     текст = 'Dobar dan.Kako ste?Ja super!Hvala'
-#     assert ли(текст) == ['Dobar dan.\n', 'Kako ste?\n', 'Ja super!\n', 'Hvala']
+def test_крајеви_реченица():
+    текст = 'Dobar dan.Kako ste?Ja super!Hvala'
+    assert ли(текст) == ['Dobar dan.\n', 'Kako ste?\n', 'Ja super!\n', 'Hvala']
 
 
-# def test_уклањање_почетног_спејса():
-#     текст = 'Dobar dan. Kako ste? Ja super! Hvala'
-#     assert ли(текст) == ['Dobar dan.\n', 'Kako ste?\n', 'Ja super!\n', 'Hvala']
+def test_уклањање_почетног_спејса():
+    текст = 'Dobar dan. Kako ste? Ja super! Hvala'
+    assert ли(текст) == ['Dobar dan.\n', 'Kako ste?\n', 'Ja super!\n', 'Hvala']
 
 
-# def test_ломљење_зареза():
-#     текст = 'Dobar dan; Kako ste, Ja super, Hvala'
-#     assert ли(текст) == ['Dobar dan;\n', 'Kako ste,\n', 'Ja super,\n', 'Hvala']
+def test_ломљење_зареза():
+    текст = 'Dobar dan; Kako ste, Ja super, Hvala'
+    assert ли(текст) == ['Dobar dan;\n', 'Kako ste,\n', 'Ja super,\n', 'Hvala']
 
 
-# def test_ломљење_двотачке():
-#     текст = 'Dobar dan: Kako ste'
-#     assert ли(текст) == ['Dobar dan:\n', 'Kako ste']
+def test_ломљење_двотачке():
+    текст = 'Dobar dan: Kako ste'
+    assert ли(текст) == ['Dobar dan:\n', 'Kako ste']
 
 
-# def test_почетни_цитат_не_ломи():
-#     текст = '"Dobar dan!"'
-#     assert ли(текст) == ['„Dobar dan!“']
+def test_почетни_цитат_не_ломи():
+    текст = '"Dobar dan!"'
+    assert ли(текст) == ['„Dobar dan!“']
 
 
-# def test_директан_говор_ломљење_двотачке():
-#     текст = '"Dobar dan: Kako ste?"'
-#     assert ли(текст) == ['„Dobar dan:“\n', '„Kako ste?“']
+def test_директан_говор_ломљење_двотачке():
+    текст = '"Dobar dan: Kako ste?"'
+    assert ли(текст) == ['„Dobar dan:“\n', '„Kako ste?“']
 
 
-# def test_директан_говор():
-#     текст = 'der sprach: "Bruder Hund. warum bist du! so traurig?"'
-#     assert ли(текст) == ['der sprach:\n', '„Bruder Hund.“\n', '„warum bist du!“\n', '„so traurig?“']
+def test_директан_говор():
+    текст = 'der sprach: "Bruder Hund. warum bist du! so traurig?"'
+    assert ли(текст) == ['der sprach:\n', '„Bruder Hund.“\n', '„warum bist du!“\n', '„so traurig?“']
 
 
-# def test_директан_говор_ломи_линију_након():
-#     текст = 'zum Hunde: "Da bleib stehen, ich will dir unterpicken," setzte sich auf den Laden'
-#     assert ли(текст) == ['zum Hunde:\n', '„Da bleib stehen,“\n', '„ich will dir unterpicken,“\n', 'setzte sich auf den Laden']
+def test_директан_говор_ломи_линију_након():
+    текст = 'zum Hunde: "Da bleib stehen, ich will dir unterpicken," setzte sich auf den Laden'
+    assert ли(текст) == ['zum Hunde:\n', '„Da bleib stehen,“\n', '„ich will dir unterpicken,“\n', 'setzte sich auf den Laden']
 
 
 # def test_директан_говор_ломи_линију_са_узвиком():
